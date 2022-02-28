97ba5605a75e20edf8725535e7c27134b5bb0d32
Leandro <leandroquiroga9514@gmail.com>
2022-02-28 18:58:00 -0300

    Custom Hooks useAuth

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,9 @@
+import { useContext } from "react"
+import { authContext } from "../context/authContext"
+
+
+export const useAuth = () => {
+  const context = useContext(authContext);
+
+  return context;
+}
